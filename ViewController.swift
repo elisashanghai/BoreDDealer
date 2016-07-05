6b919900c033c3fb90e19f88bfef398677fae324
elisashanghai <elisa.shanghai@gmail.com>
2016-07-05 14:45:03 -0700

    Start
    Empty new project.

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BoreDDealer
+//
+//  Created by Yang Song on 7/5/16.
+//  Copyright © 2016 WhenYouBored. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
