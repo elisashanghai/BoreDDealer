63de4025029cc7cf4c6e9a6931b8aff3431e8c81
elisashanghai <elisa.shanghai@gmail.com>
2016-07-07 09:27:04 -0700

    simple face recognition
    only recognize the face under some conditions: face should be big
    enough, no laugh.

--- BoreDDealer/Helpers/ActivityHelper.swift
+++ BoreDDealer/Helpers/ActivityHelper.swift
@@ -6,22 +6,22 @@
 //  Copyright © 2016 WhenYouBored. All rights reserved.
 //
 
-import Foundation
-
-struct Activity {
-    var list30Min = ["Read a new book!", "Make a bucket list of 20 things you want to do before you die!", "Start a watching a new TV series!", "Bake something!", "Write a letter for someone you care about", "Go biking in a park nearby you", "Take a 30 minute nap and feel refreshed!", "Learn about a sport that's popular in another country besides the one you reside in!", "Make a drawing of the first thing you see when you step outside", "Make a video of something you'd like to tell your 'future self'!","Learn a new type of dance! (something that's foreign to where you're from!)"]
-    
-    var listHour = ["Watch a movie that you've never watched before!", "Cook a special meal for your loved one(s)!", "'Spring' cleaning time while having a dance party for 1!", "Learn the basics of how to code in iOS or Android!", "Make a scrapbook of your most memorable times!", "Try doing Zumba!", "Treat yourself to a 'Me Day'! Go spoil yourself!", "Play one of your favorite games from your childhood!", "Roll up your extra change and come up with a creative way to spend it! It doesn't matter how litte you have!", "Plan the 'perfect' vacation getaway that you'd like to go on!"]
-
-    init(list30: list30Min, list1Hr: listHour) {
-        self.list30 = list30
-        self.list1Hr = list1Hr
-    }
-
-}
-
-class Activity {
-    var activityFor30 = [list30Min]()
-    var activityForHour = [listHour]()
-    
-}
+//import Foundation
+//
+//struct Activity {
+//    var list30Min = ["Read a new book!", "Make a bucket list of 20 things you want to do before you die!", "Start a watching a new TV series!", "Bake something!", "Write a letter for someone you care about", "Go biking in a park nearby you", "Take a 30 minute nap and feel refreshed!", "Learn about a sport that's popular in another country besides the one you reside in!", "Make a drawing of the first thing you see when you step outside", "Make a video of something you'd like to tell your 'future self'!","Learn a new type of dance! (something that's foreign to where you're from!)"]
+//    
+//    var listHour = ["Watch a movie that you've never watched before!", "Cook a special meal for your loved one(s)!", "'Spring' cleaning time while having a dance party for 1!", "Learn the basics of how to code in iOS or Android!", "Make a scrapbook of your most memorable times!", "Try doing Zumba!", "Treat yourself to a 'Me Day'! Go spoil yourself!", "Play one of your favorite games from your childhood!", "Roll up your extra change and come up with a creative way to spend it! It doesn't matter how litte you have!", "Plan the 'perfect' vacation getaway that you'd like to go on!"]
+//
+//    init(list30: list30Min, list1Hr: listHour) {
+//        self.list30 = list30
+//        self.list1Hr = list1Hr
+//    }
+//
+//}
+//
+//class Activity {
+//    var activityFor30 = [list30Min]()
+//    var activityForHour = [listHour]()
+//    
+//}
--- BoreDDealer/StartViewController.swift
+++ BoreDDealer/StartViewController.swift
@@ -7,19 +7,64 @@
 //
 
 import UIKit
+import CoreImage
 
-class StartViewController: UIViewController {
+class StartViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate{
 
+    @IBOutlet weak var imagePicked: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func takeSelfie(sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.Camera;
+            imagePicker.allowsEditing = false
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+        }
+        
+    }
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        let ciImage = CIImage(CGImage: image.CGImage!)
+        
+        let options = [CIDetectorAccuracy: CIDetectorAccuracyHigh]
+        let faceDetector = CIDetector(ofType: CIDetectorTypeFace, context: nil, options: options)
+        
+        let faces = faceDetector.featuresInImage(ciImage)
+        
+        if let face = faces.first as? CIFaceFeature {
+            print("Found face at \(face.bounds)")
+            
+            if face.hasLeftEyePosition {
+                print("Found left eye at \(face.leftEyePosition)")
+            }
+            
+            if face.hasRightEyePosition {
+                print("Found right eye at \(face.rightEyePosition)")
+            }
+            
+            if face.hasMouthPosition {
+                print("Found mouth at \(face.mouthPosition)")
+            }
+        }
+        imagePicked.image = image
+        self.dismissViewControllerAnimated(true, completion: nil);
+    }
+    
+    
 
 }
+
+
+
+// MARK: Tab Bar Delegate
 
