21b08929e218f4d4d03dd5675b53de29deb949d5
elisashanghai <elisa.shanghai@gmail.com>
2016-07-07 17:00:29 -0700

    face detection with rectangle
    front camera

--- BoreDDealer/StartViewController.swift
+++ BoreDDealer/StartViewController.swift
@@ -8,6 +8,15 @@
 
 import UIKit
 import CoreImage
+import AVFoundation
+
+class MyCanvas: UIView {
+    override func drawRect(rect: CGRect) {
+        UIColor.greenColor().setFill()
+        let path = UIBezierPath(rect: self.bounds)
+        path.fill()
+    }
+}
 
 class StartViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate{
 
@@ -24,25 +33,66 @@ class StartViewController: UIViewController, UIImagePickerControllerDelegate,UIN
     }
     
     @IBAction func takeSelfie(sender: AnyObject) {
+        
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
             let imagePicker = UIImagePickerController()
             imagePicker.delegate = self
             imagePicker.sourceType = UIImagePickerControllerSourceType.Camera;
             imagePicker.allowsEditing = false
+            if UIImagePickerController.isCameraDeviceAvailable(UIImagePickerControllerCameraDevice.Front){
+                imagePicker.cameraDevice = UIImagePickerControllerCameraDevice.Front
+            }
             self.presentViewController(imagePicker, animated: true, completion: nil)
         }
         
     }
+    
+    
+    
+    
     func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
         let ciImage = CIImage(CGImage: image.CGImage!)
         
-        let options = [CIDetectorAccuracy: CIDetectorAccuracyHigh]
-        let faceDetector = CIDetector(ofType: CIDetectorTypeFace, context: nil, options: options)
+ 
+        let faceDetector = CIDetector(ofType:CIDetectorTypeFace, context:nil, options:[CIDetectorAccuracy: CIDetectorAccuracyHigh])
+        
+        
         
         let faces = faceDetector.featuresInImage(ciImage)
         
         if let face = faces.first as? CIFaceFeature {
             print("Found face at \(face.bounds)")
+            
+            let inputImageSize = ciImage.extent.size
+            var transform = CGAffineTransformIdentity
+            transform = CGAffineTransformScale(transform, 0.7, -0.7)
+            transform = CGAffineTransformTranslate(transform, 0, -inputImageSize.height)
+            
+            for faceFeature in faces {
+                var faceViewBounds = CGRectApplyAffineTransform(faceFeature.bounds, transform)
+                // 2.
+                let scaleTransform = CGAffineTransformMakeScale(0.5, 0.5)
+                faceViewBounds = CGRectApplyAffineTransform(faceViewBounds, scaleTransform)
+                
+                let faceView = UIView(frame: faceViewBounds)
+                faceView.layer.borderColor = UIColor.orangeColor().CGColor
+                faceView.layer.borderWidth = 2
+                
+                imagePicked.addSubview(faceView)
+            }
+
+            
+//            let viewRect = CGRect(x: 50, y: 50, width: face.bounds.width, height: face.bounds.height)
+//            let faceView = MyCanvas(frame: viewRect)
+//            imagePicked.addSubview(faceView)
+            
+            
+//            let cropSquare = CGRectMake(face.bounds.minX, face.bounds.minY, face.bounds.width, face.bounds.height)
+//            let imageRef = CGImageCreateWithImageInRect(image.CGImage, cropSquare);
+//            print("minX \(face.bounds.minX) minY \(face.bounds.minY) width \(face.bounds.width) height \(face.bounds.height)")
+//            
+          
+//            imagePicked.image = UIImage(CGImage: imageRef!)
             
             if face.hasLeftEyePosition {
                 print("Found left eye at \(face.leftEyePosition)")
@@ -55,7 +105,12 @@ class StartViewController: UIViewController, UIImagePickerControllerDelegate,UIN
             if face.hasMouthPosition {
                 print("Found mouth at \(face.mouthPosition)")
             }
+            
+            
+
         }
+        
+        
         imagePicked.image = image
         self.dismissViewControllerAnimated(true, completion: nil);
     }
